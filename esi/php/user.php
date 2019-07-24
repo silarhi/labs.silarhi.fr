22c1c126309f1741a564312526b96e2da9e76b3c
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2019-07-24 23:06:00 +0200

    :art: remove unecessary parenthesis

--- esi/php/user.php
+++ esi/php/user.php
@@ -4,7 +4,7 @@
 <br />
 <div class="private p-2">
 	<?php if($_SESSION['logged'] ?? false): ?>
-		Dernière connexion il y a <?= time() - ($_SESSION['last_login']) ?>s
+		Dernière connexion il y a <?= time() - $_SESSION['last_login'] ?>s
 	<?php else: ?>
 		Pas de dernière connexion
 	<?php endif; ?>
