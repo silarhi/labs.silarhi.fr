a3ad64621af9a44b06a23d8948ff44df35c6f89b
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2019-11-29 13:16:02 +0100

    :art: Upgraded deploy script

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 set -e
 
-IMAGE=$1
-
 #Download new image version
-docker pull ${IMAGE}
+docker-compose pull
 
 #Set maintenance mode to perform critical operations (database, upgrades, ...)
 APP_MAINTENANCE=1 docker-compose up -d
-#i.e: docker-compose exec app bin/console doctrine:migration:migrate -n
-sleep 2; #for demo purpose
+#i.e: docker-compose exec -T app bin/console doctrine:migration:migrate -n
+sleep 10; #for demo purpose
 
 #back to prod
 docker-compose up -d
