9d27829af68de9e868f5f314707275b9f0c3eb7b
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2020-01-11 14:26:59 +0100

    Upgrade to BS 4.4

--- esi/php/index.php
+++ esi/php/index.php
@@ -7,7 +7,7 @@
 <head>
 	<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <style>
     	.public { border: 2px dashed green; }
     	.private { border: 2px dashed red; }
