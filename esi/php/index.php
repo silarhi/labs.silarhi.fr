db11047c9c5085fe1ba9d6fd22cab8bc477597a5
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2022-01-04 19:26:52 +0100

    Micro improve UI

--- esi/php/index.php
+++ esi/php/index.php
@@ -7,14 +7,14 @@ header("X-Reverse-Proxy-TTL: 10");
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <style>
         .public {
-            border: 2px dashed green;
+            border: 2px dashed #198754;
         }
 
         .private {
-            border: 2px dashed red;
+            border: 2px dashed #dc3545;
         }
 
         .cached {
