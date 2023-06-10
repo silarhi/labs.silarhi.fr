42354d059706b39e2b75aa7f60fdc5c719603b01
Guillaume Sainthillier <guillaume@silarhi.fr>
2023-06-10 13:11:51 +0000

    commit from prod

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -2,12 +2,12 @@
 set -e
 
 #Download new image version
-docker-compose pull
+docker compose pull
 
 #Set maintenance mode to perform critical operations (database, upgrades, ...)
-APP_MAINTENANCE=1 docker-compose up -d
+APP_MAINTENANCE=1 docker compose up -d
 #i.e: docker-compose exec -T app bin/console doctrine:migration:migrate -n
 sleep 10; #for demo purpose
 
 #back to prod
-docker-compose up -d
+docker compose up -d
